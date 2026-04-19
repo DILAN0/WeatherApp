09c31d5968f9aded5a3f40f6c5c8dd65312dfd3b
Your Name <you@example.com>
2026-04-19 12:05:10 +0300

    Add: Добавил структурирование данных при получении данных с API

---
+++ models.py
@@ -0,0 +1,7 @@
+class WeatherReport:
+    def __init__(self, date, temp, humidity, wind_speed, desc):
+        self.date = date
+        self.temp = temp
+        self.humidity = humidity
+        self.wind_speed = wind_speed
+        self.desc = desc
